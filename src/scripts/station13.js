95b22bc04db82e94e5169f255e3ed9399cf1e52c
DaisukeMatsuda <daimix@hotmail.com>
2025-10-06 16:27:22 +0900

    station13の変更

--- src/scripts/station13.js
+++ src/scripts/station13.js
@@ -1,4 +1,14 @@
 function changeBackgroundColor(e) {
   // ここに背景色を変える処理を書く。
+  // チェックボックスの要素を取得
+  const checkbox = document.getElementById('check');
+  const textElement = document.getElementById('text');
 
+  // チェックされてるかどうかを調べる
+  checkbox.checked  // これがtrue（チェック済み）かfalse（未チェック）を返すよ
+  if (checkbox.checked) {
+    textElement.style.backgroundColor = "red";
+  } else {
+    textElement.style.backgroundColor = "white";
+  }
 }
